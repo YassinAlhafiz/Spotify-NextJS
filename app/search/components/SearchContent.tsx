7228ffb818f38875f5c7abaa4d6eb6d04026e8e5
YassinAlhafiz <alhafiz.yassin@gmail.com>
2023-09-24 18:39:06 -0400

    Implemented like song functionality

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,4 +1,5 @@
 "use client";
+import LikeButton from "@/components/LikeButton";
 import MediaItem from "@/components/MediaItem";
 import { Song } from "@/types";
 import { FC } from "react";
@@ -22,7 +23,7 @@ const SearchContent: FC<SearchContentProps> = ({ songs }) => {
 					<div className="flex-1">
 						<MediaItem key={song.id} data={song} onClick={() => {}} />
 					</div>
-					{/* Add Like Button Here */}
+					<LikeButton songId={song.id} />
 				</div>
 			))}
 		</div>
