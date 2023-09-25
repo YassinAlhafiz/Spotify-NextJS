dc10a7700bcb1a242ea9071641da64ac97161d45
YassinAlhafiz <alhafiz.yassin@gmail.com>
2023-09-24 21:30:52 -0400

    Building Player Functionality

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import SongItems from "@/components/SongItems";
+import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types";
 import { FC } from "react";
 
@@ -9,13 +10,18 @@ interface PageContentProps {
 }
 
 const PageContent: FC<PageContentProps> = ({ songs }) => {
+	const onPlay = useOnPlay(songs);
 	if (songs.length === 0) {
 		return <div className="mt-4 text-neutral-400">No Songs Available</div>;
 	}
 	return (
 		<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-8 gap-4 mt-4">
 			{songs.map((song) => (
-				<SongItems key={song.id} onClick={() => {}} data={song} />
+				<SongItems
+					key={song.id}
+					onClick={(id: string) => onPlay(id)}
+					data={song}
+				/>
 			))}
 		</div>
 	);
